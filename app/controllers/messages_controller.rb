e59721f519f7933f8da870ff100564f3ae55db99
CStory <chasestory@outlook.com>
2018-09-21 19:13:47 -0600

    finalized the messages feature

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,32 +10,43 @@ class MessagesController < ApplicationController
   end
 
   def new
-    @message = Message.new
+    @message = current_user.messages.build
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.build(message_params)
     if @message.save
       redirect_to root_path
     else 
       render 'new'
     end
   end
+
+  def edit 
+
+  end
   
   def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @message.destroy
+    redirect_to root_path
   end
 
 
   private
 
-    def message_params
-      params.require(:message).permit(:title, :description)
-    end
+  def message_params
+    params.require(:message).permit(:title, :description)
+  end
 
-    def find_message
-      @message = Message.find(params[:id])
-    end
+  def find_message
+    @message = Message.find(params[:id])
+  end
 end
